735832828d4096aa824ed62e8e46f493eb2466aa
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2024-12-28 12:39:59 +0400

    removed , auto: false

--- models/Product.js
+++ models/Product.js
@@ -2,29 +2,29 @@ const mongoose = require('mongoose');
 
 // Generating Schema
 const productSchema = new mongoose.Schema({
-    id: {type: Number, required: true, unique: true},
+    id: { type: Number, required: true, unique: true},
     productName : { type: String, required: true, unique: true },
     category: { type: String, required: true},
     description : { type: String, required: true, unique: true },
     image : { type: String, required: true },
     price: { type: Number, required: true },
     stock: { type: Number, required: true }
-}, { _id: false, auto: false });
+}, { _id: false});
 
-// Pre-save hook to generate customId
+// Pre-save hook to generate custom ID
 productSchema.pre('save', async function (next) {
     if (!this.isNew) {
-        console.log("Inside !this.isNew");
-        return next();
-    }     // Only run for new documents
+        console.log("Not a new document");
+        return next(); // Only run for new documents
+    }
 
     try {
-        console.log("Inside product.isNew try");
+        console.log("Inside pre-save hook");
         const lastProduct = await mongoose.model('Product').findOne().sort({ id: -1 });
         this.id = lastProduct ? lastProduct.id + 1 : 1; // Increment the ID or start at 1
         next();
     } catch (error) {
-        console.log("Inside product.isNew catch");
+        console.log("Error in pre-save hook:", error);
         next(error);
     }
 });
