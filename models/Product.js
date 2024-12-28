291c461328599de2a9b34e076269331bba91036e
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2024-12-28 11:30:33 +0400

    Added consoles to debugg

--- models/Product.js
+++ models/Product.js
@@ -13,13 +13,18 @@ const productSchema = new mongoose.Schema({
 
 // Pre-save hook to generate customId
 productSchema.pre('save', async function (next) {
-    if (!this.isNew) return next(); // Only run for new documents
+    if (!this.isNew) {
+        console.log("Inside !this.isNew");
+        return next();
+    }     // Only run for new documents
 
     try {
+        console.log("Inside product.isNew try");
         const lastProduct = await mongoose.model('Product').findOne().sort({ id: -1 });
         this.id = lastProduct ? lastProduct.id + 1 : 1; // Increment the ID or start at 1
         next();
     } catch (error) {
+        console.log("Inside product.isNew catch");
         next(error);
     }
 });
