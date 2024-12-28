c323a8e4fa1e0d19aa0a13934e53c4f1ddc50a74
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2024-12-28 13:34:28 +0400

    modified

--- models/Product.js
+++ models/Product.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 // Generating Schema
 const productSchema = new mongoose.Schema(
   {
-    id: { type: Number, unique: true },
+    id: { type: Number, required: true, unique: true },
     productName: { type: String, required: true, unique: true },
     category: { type: String, required: true },
     description: { type: String, required: true, unique: true },
@@ -13,29 +13,6 @@ const productSchema = new mongoose.Schema(
   },
   { _id: false }
 );
-
-// Pre-save hook to generate custom ID
-productSchema.pre("save", async function (next) {
-  console.log("Inside pre-save hook try");
-  console.log("This : ", this);
-  if (!this.isNew) {
-    console.log("Not a new document");
-    return next(); // Only run for new documents
-  }
-
-  try {
-    console.log("Inside pre-save hook try");
-    const lastProduct = await mongoose
-      .model("Product")
-      .findOne()
-      .sort({ id: -1 });
-    this.id = lastProduct ? lastProduct.id + 1 : 1; // Increment the ID or start at 1
-    next();
-  } catch (error) {
-    console.log("Error in pre-save hook:", error);
-    next(error);
-  }
-});
 
 // Creating the model and exporting it
 const Product = mongoose.model("Product", productSchema); // Model registration
