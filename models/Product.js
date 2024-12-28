83f75bc7e67570e100058ae3bdc34f2291bbcc4b
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2024-12-28 12:52:42 +0400

    added new console

--- models/Product.js
+++ models/Product.js
@@ -1,34 +1,41 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Generating Schema
-const productSchema = new mongoose.Schema({
-    id: { type: Number, required: true, unique: true},
-    productName : { type: String, required: true, unique: true },
-    category: { type: String, required: true},
-    description : { type: String, required: true, unique: true },
-    image : { type: String, required: true },
+const productSchema = new mongoose.Schema(
+  {
+    id: { type: Number, required: true, unique: true },
+    productName: { type: String, required: true, unique: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true, unique: true },
+    image: { type: String, required: true },
     price: { type: Number, required: true },
-    stock: { type: Number, required: true }
-}, { _id: false});
+    stock: { type: Number, required: true },
+  },
+  { _id: false }
+);
 
 // Pre-save hook to generate custom ID
-productSchema.pre('save', async function (next) {
-    if (!this.isNew) {
-        console.log("Not a new document");
-        return next(); // Only run for new documents
-    }
+productSchema.pre("save", async function (next) {
+  console.log("Inside pre-save hook try");
+  if (!this.isNew) {
+    console.log("Not a new document");
+    return next(); // Only run for new documents
+  }
 
-    try {
-        console.log("Inside pre-save hook");
-        const lastProduct = await mongoose.model('Product').findOne().sort({ id: -1 });
-        this.id = lastProduct ? lastProduct.id + 1 : 1; // Increment the ID or start at 1
-        next();
-    } catch (error) {
-        console.log("Error in pre-save hook:", error);
-        next(error);
-    }
+  try {
+    console.log("Inside pre-save hook try");
+    const lastProduct = await mongoose
+      .model("Product")
+      .findOne()
+      .sort({ id: -1 });
+    this.id = lastProduct ? lastProduct.id + 1 : 1; // Increment the ID or start at 1
+    next();
+  } catch (error) {
+    console.log("Error in pre-save hook:", error);
+    next(error);
+  }
 });
 
 // Creating the model and exporting it
-const Product = mongoose.model('Product', productSchema); // Model registration
+const Product = mongoose.model("Product", productSchema); // Model registration
 module.exports = Product;
