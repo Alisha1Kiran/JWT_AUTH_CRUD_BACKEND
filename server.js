4dedc9634f80608849bf6e54574ccc8d80c35587
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2025-01-03 10:47:57 +0400

    Minor modification

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const userRouter = require("./routes/userRouter");
 const productsRouter = require("./routes/productsRouter");
-const cors = require('cors');
+const cors = require("cors");
 require("dotenv").config();
 
 const server = new express();
@@ -10,18 +10,21 @@ const port = process.env.PORT;
 const mongooseUrl = process.env.MONGODB_URL;
 
 // Use cors middleware
-app.use(cors({
-  origin: 'http://localhost:5173', // Allow only your frontend
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-}));
+server.use(
+  cors({
+    origin: "http://localhost:5173", // Allow only your frontend
+    methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+  })
+);
 
 server.get("/", (req, res) => {
   res.send("Hello mern stack app !");
 });
 
 // MongoDB Connection
-mongoose.connect(mongooseUrl)
+mongoose
+  .connect(mongooseUrl)
   .then(() => {
     console.log("DB connected successfully");
   })
