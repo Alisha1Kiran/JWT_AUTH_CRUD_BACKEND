6238fd32e4a1d854d3c7f2fdb584dfb037ef98ce
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2025-01-04 11:06:29 +0400

    modified code to send user details in response in auth call

--- controller/userController.js
+++ controller/userController.js
@@ -1,168 +1,175 @@
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken')
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 // Fetch all users
-const getAllUsers = async (req,res) => {
-    if(req.user.role !== "admin") return res.status(403).json({ message: "Access denied: Admins only" });
-    
-    try {
-        const usersList = await User.find();
-        res.json(usersList);
-      } catch (err) {
-        res.status(500).json({ error: err.message });
-      }
-}
+const getAllUsers = async (req, res) => {
+  if (req.user.role !== "admin")
+    return res.status(403).json({ message: "Access denied: Admins only" });
+
+  try {
+    const usersList = await User.find();
+    res.json(usersList);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
 
 // Adding user details to db
 const addUser = async (req, res) => {
-    try {
-        if (req.body) {
-            // Exteract data from the request body
-            const { fullName, email, password} = req.body;
+  try {
+    if (req.body) {
+      // Exteract data from the request body
+      const { fullName, email, password } = req.body;
 
-            // Hashing the password
-            const hashedPassword = await bcrypt.hash(password, 12);
+      // Hashing the password
+      const hashedPassword = await bcrypt.hash(password, 12);
 
-            // Create a new user document
-            const newUser = new User({
-                fullName,
-                email,
-                password : hashedPassword,
-                role : 'user'
-            })
-            
-            // posrting user details to db
-            await newUser.save()
-                .then( (savedUser) => {
-                    res.status(200).json({
-                        message: 'Successfully added new user',
-                        user: savedUser
-                    })
-                } )
-                .catch( (error) => {
-                    res.status(500).json({
-                        message: 'Error adding user',
-                        error: error.message // Send the error message in the response
-                    });
-                } )
-        }
-    } catch (error) {
-        console.error(`Error is : ${error}`)
-        res.status(500).send('Internal Server Error');
+      // Create a new user document
+      const newUser = new User({
+        fullName,
+        email,
+        password: hashedPassword,
+        role: "user",
+      });
+
+      // posrting user details to db
+      await newUser
+        .save()
+        .then((savedUser) => {
+          res.status(200).json({
+            message: "Successfully added new user",
+            user: savedUser,
+          });
+        })
+        .catch((error) => {
+          res.status(500).json({
+            message: "Error adding user",
+            error: error.message, // Send the error message in the response
+          });
+        });
     }
-}
+  } catch (error) {
+    console.error(`Error is : ${error}`);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 // Login
 const userLogin = async (req, res) => {
-    const {email, password} = req.body;
+  const { email, password } = req.body;
 
-    try {
-        // Using email whether user exists in DB
-        const user = await User.findOne({ email });
-        console.log(`User with email ${email} exists`);
-        console.log(`User : ${user}`);
+  try {
+    // Using email whether user exists in DB
+    const user = await User.findOne({ email });
+    console.log(`User with email ${email} exists`);
+    console.log(`User : ${user}`);
 
-        if (!user) return res.status(404).json({ message: "User not found" });
+    if (!user) return res.status(404).json({ message: "User not found" });
 
-        const isPasswordMatch = await bcrypt.compare(password, user.password);
-        
-        if (!isPasswordMatch) return res.status(401).json({ message: "Invalid credentials" });
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
 
-        // Generate JWT Token
-        const token = jwt.sign(
-            { email: user.email, role: user.role }, // Payload
-            process.env.JWT_SECRET,                      // Secret key
-            { expiresIn: '1h' }                     // Options (e.g., token expiration)
-        );
+    if (!isPasswordMatch)
+      return res.status(401).json({ message: "Invalid credentials" });
 
-        // Send the token to the client
-        res.status(200).json({ 
-            message: 'Login successful', 
-            token: token 
-        });
+    // Generate JWT Token
+    const token = jwt.sign(
+      { email: user.email, role: user.role }, // Payload
+      process.env.JWT_SECRET, // Secret key
+      { expiresIn: "1h" } // Options (e.g., token expiration)
+    );
 
-    } catch (error) {
-        console.error(`Error is : ${error}`);
-        res.status(500).send('Internal Server Error');
-    }
-}
+    // Send the token to the client
+    res.status(200).json({
+      message: "Login successful",
+      token: token,
+      user: user,
+    });
+  } catch (error) {
+    console.error(`Error is : ${error}`);
+    res.status(500).send("Internal Server Error");
+  }
+};
 
 // Get user by id
-const getUserById = async (req,res) => {
-    const userId = req.params.id;
+const getUserById = async (req, res) => {
+  const userId = req.params.id;
 
-    try {
-        const userDetails = await User.findById(userId)
+  try {
+    const userDetails = await User.findById(userId);
 
-        if (!userDetails) return res.status(402).json({
-            message: "User not found"
-        })
+    if (!userDetails)
+      return res.status(402).json({
+        message: "User not found",
+      });
 
-        res.status(200).json({
-            message: "User found successfully",
-            user: userDetails
-        })
-        
-    } catch (error) {
-        res.status(500).json({
-            message: 'Error finding user',
-            error: error.message // Send the error message in the response
-        });
-    }
-}
+    res.status(200).json({
+      message: "User found successfully",
+      user: userDetails,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error finding user",
+      error: error.message, // Send the error message in the response
+    });
+  }
+};
 
 // Update user data
 const updateUserData = async (req, res) => {
+  const userId = req.params.id; // User ID from URL
+  const userDataToUpdate = req.body; // Fields to update
 
-    const userId = req.params.id; // User ID from URL
-    const userDataToUpdate = req.body; // Fields to update
+  try {
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      userDataToUpdate,
+      { new: true, runValidators: true } // Return updated document and validate changes
+    );
 
-    try {
-        const updatedUser = await User.findByIdAndUpdate(
-            userId,
-            userDataToUpdate,
-            { new: true, runValidators: true } // Return updated document and validate changes
-        );
-        
-        if (!updatedUser) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        res.status(200).json({
-            message: 'User updated successfully',
-            updatedUser,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Error updating user details',
-            error: error.message,
-        });
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
+
+    res.status(200).json({
+      message: "User updated successfully",
+      updatedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error updating user details",
+      error: error.message,
+    });
+  }
+};
 
 // Delete user
 const deleteUser = async (req, res) => {
+  const userId = req.params.id; // User ID from URL
+  try {
+    const deleteUser = await User.findByIdAndDelete(userId);
 
-    const userId = req.params.id; // User ID from URL
-    try {
-        const deleteUser = await User.findByIdAndDelete(userId);
-
-        if (!deleteUser) {
-            return res.status(404).json({ message: "User not found" });
-        }
-
-        res.status(200).json({
-            message: 'User deleted successfully',
-            deleteUser,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: 'Error deleting product',
-            error: error.message,
-        });
+    if (!deleteUser) {
+      return res.status(404).json({ message: "User not found" });
     }
-}
 
+    res.status(200).json({
+      message: "User deleted successfully",
+      deleteUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Error deleting product",
+      error: error.message,
+    });
+  }
+};
 
-module.exports = {addUser, userLogin, getAllUsers, getUserById, updateUserData, deleteUser}
+module.exports = {
+  addUser,
+  userLogin,
+  getAllUsers,
+  getUserById,
+  updateUserData,
+  deleteUser,
+};
