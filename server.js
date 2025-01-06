c8b7013d531bea23a5edc886295e1adc9e61f7f4
Alisha1Kiran <61532157+Alisha1Kiran@users.noreply.github.com>
2025-01-06 18:04:34 +0400

    Fix for multiple core polices

--- server.js
+++ server.js
@@ -9,15 +9,25 @@ const server = new express();
 const port = process.env.PORT;
 const mongooseUrl = process.env.MONGODB_URL;
 
+const allowedOrigins = [
+  "http://localhost:5173",  // Local development
+  "https://jwt-auth-crud-frontend.onrender.com"  // frontend production domain
+];
 // Use cors middleware
 server.use(
   cors({
-    origin: "http://localhost:5173", // Allow only your frontend
+    origin: (origin, callback) => {
+      // Allow requests with no origin (e.g., mobile apps or curl requests)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
-    allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+    allowedHeaders: ["Content-Type", "Authorization"] // Allowed headers
   })
 );
-
 server.get("/", (req, res) => {
   res.send("Hello mern stack app !");
 });
